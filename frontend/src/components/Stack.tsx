0ae7d40102162c1bd2b4bb9b4d626047fd0e82e1
Simon Hochholzer <simon@symmetronic.com>
2025-10-25 07:31:20 +0200

    refactor: simplify stack component

--- frontend/src/components/Stack.tsx
+++ frontend/src/components/Stack.tsx
@@ -5,6 +5,9 @@ export interface StackProps {
   spacing?: StackTypeMap["props"]["spacing"];
 }
 
-export function Stack({ children, spacing }: PropsWithChildren<StackProps>) {
-  return <MuiStack {...(spacing && { spacing })}>{children}</MuiStack>;
+export function Stack({
+  children,
+  spacing = 0,
+}: PropsWithChildren<StackProps>) {
+  return <MuiStack spacing={spacing}>{children}</MuiStack>;
 }
